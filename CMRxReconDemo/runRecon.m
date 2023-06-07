08ced1b780e7ad23c33a5095632652f6668ad0b8
ChengYanHuPI <114715003+ChengYanHuPI@users.noreply.github.com>
2023-06-07 23:10:43 +0800

    Add files via upload

--- CMRxReconDemo/runRecon.m
+++ CMRxReconDemo/runRecon.m
@@ -21,30 +21,43 @@ for ind0 = 1:3
             continue;
         end
         disp(['Progress start for subject ',num2str(ind1-k)]);
-        
-        file_name = FileList(ind1).name;
-        dataPath = strcat(mainDataPath,'/',file_name,'/',filetype,'.mat');
-        load(dataPath); % load raw kspace data
-        kspace = eval(char(AFname(ind0)));
-        % to reduce the computing burden and space, we only evaluate the central 2 slices
-        % For cine: use the first 3 time frames for ranking!
-        % For mapping: we need all weighting for ranking!
-        [sx,sy,~,sz,t] = size(kspace);
-        if strcmp(filetype,'cine_lax') || strcmp(filetype,'cine_sax')
-            reconImg = ChallengeRecon(kspace(:,:,:,round(sz/2)-1:round(sz/2),1:3), type, reconType, imgShow);
-            % crop the middle 1/6 of the original image for ranking
-            img4ranking = single(crop(abs(reconImg),[round(sx/3),round(sy/2),2,3]));
-        else
-            reconImg = ChallengeRecon(kspace(:,:,:,round(sz/2)-1:round(sz/2),:), type, reconType, imgShow);
-            % crop the middle 1/6 of the original image for ranking
-            img4ranking = single(crop(abs(reconImg),[round(sx/3),round(sy/2),2,t]));
+        try
+            file_name = FileList(ind1).name;
+            dataPath = strcat(mainDataPath,'/',file_name,'/',filetype,'.mat');
+            load(dataPath); % load raw kspace data
+            kspace = eval(char(AFname(ind0)));
+            if strcmp(coilInfo,'SingleCoil/')
+                [sx,sy,sz,t] = size(kspace);
+                kspaceMulti = zeros(sx,sy,1,sz,t);
+                kspaceMulti(:,:,1,:,:) = kspace;
+                kspace = kspaceMulti;
+            end
+            % to reduce the computing burden and space, we only evaluate the central 2 slices
+            % For cine: use the first 3 time frames for ranking!
+            % For mapping: we need all weighting for ranking!
+            [sx,sy,~,sz,t] = size(kspace);
+            if strcmp(filetype,'cine_lax') || strcmp(filetype,'cine_sax')
+                if sz < 3
+                    reconImg = ChallengeRecon(kspace(:,:,:,:,1:3), type, reconType, imgShow);
+                else
+                    reconImg = ChallengeRecon(kspace(:,:,:,round(sz/2)-1:round(sz/2),1:3), type, reconType, imgShow);
+                end
+                % crop the middle 1/6 of the original image for ranking
+                img4ranking = single(crop(abs(reconImg),[round(sx/3),round(sy/2),2,3]));
+            else
+                reconImg = ChallengeRecon(kspace(:,:,:,round(sz/2)-1:round(sz/2),:), type, reconType, imgShow);
+                % crop the middle 1/6 of the original image for ranking
+                img4ranking = single(crop(abs(reconImg),[round(sx/3),round(sy/2),2,t]));
+            end
+
+            % mkdir for saving
+            if exist(strcat(savePath,'/',file_name), 'dir') ~= 7
+                mkdir(strcat(savePath,'/',file_name));
+            end
+            save(strcat(savePath,'/',file_name,'/',filetype,'.mat'),'img4ranking');        
+            disp(strcat(char(AFtype(ind0))," reconstructed successfully!"));
+        catch
+            disp(strcat(char(dataPath)," missing!"));
         end
-        
-        % mkdir for saving
-        if exist(strcat(savePath,'/',file_name), 'dir') ~= 7
-            mkdir(strcat(savePath,'/',file_name));
-        end
-        save(strcat(savePath,'/',file_name,'/',filetype,'.mat'),'img4ranking');        
     end
-    disp(strcat(char(AFtype(ind0))," reconstructed successfully!"));
 end
