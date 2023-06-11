fdc777e352c1ce97af51c11562d3dd03f258233e
ChengYanHuPI <114715003+ChengYanHuPI@users.noreply.github.com>
2023-06-11 12:21:00 +0800

    Add files via upload

--- Evaluation/MainLoadFun.py
+++ Evaluation/MainLoadFun.py
@@ -12,7 +12,19 @@ import os
 #import numpy as np
 from loadFun import loadmat
 
+# Load mat
 data_dir = 'data'
 mat_file = os.path.join(data_dir, 'example.mat')
 
 data = loadmat(mat_file)
+
+# write mat
+# Create the MATLAB data file
+mat_file = h5py.File('data.mat', 'w')
+
+# Save the data to the file
+mat_file.create_dataset('data', data= mat_file)  # Create dataset and save data
+# Replace 'mat_file' with the desired dataset name
+
+# Close the file
+mat_file.close()
--- Evaluation/loadFun.py
+++ Evaluation/loadFun.py
@@ -7,56 +7,19 @@ Load .mat data using python
 @author: c.y.wang
 """
 
-import scipy.io as scio
-import numpy as np
-from coil_combine import rss_complex
-
+import h5py
 def loadmat(filename):
-    # 读取.mat 文件
-    mat_file = scio.loadmat(filename)
-    # 获取数据集
-    dataset = mat_file['img4ranking']
-    return dataset
-
-
-def ifft2c(x):
-    # 获取 x 的 shape
-    S = np.shape(x)
-
-    # 计算缩放因子
-    fctr = S[0] * S[1]
-
-    # 重塑 x
-    x = np.reshape(x, (S[0], S[1], np.prod(S[2:])))
-
-    # 初始化结果数组
-    res = np.zeros(np.shape(x), dtype=complex)
-
-    # 对每一个通道执行二维傅立叶逆变换
-    for n in range(np.shape(x)[2]):
-        res[:,:,n] = np.sqrt(fctr) * np.fft.ifftshift(np.fft.ifft2(np.fft.fftshift(x[:,:,n])))
-
-    # 重塑结果数组
-    res = np.reshape(res, S)
-
-    return res
-
-def multicoilkdata2img(dataset):
-    k_space =np.transpose(dataset,[4,3,2,1,0])
-    # 将 k-space 转换为复数数组
-    k_space = k_space['real'] + 1j * k_space['imag']
-    recon = ifft2c(k_space)
-    # 对 k-space 应用快速傅里叶变换
-    recon = abs(rss_complex(recon, 2))
-    return recon
-
-def kdata2img(dataset):
-    k_space = np.transpose(dataset, [3, 2, 1, 0])
-    # 将 k-space 转换为复数数组
-    k_space = k_space['real'] + 1j * k_space['imag']
-    # 对 k-space 应用快速傅里叶变换
-    recon = abs(ifft2c(k_space))
-    return recon
+    """
+    Load Matlab v7.3 format .mat file using h5py.
+    """
+    with h5py.File(filename, 'r') as f:
+        data = {}
+        for k, v in f.items():
+            if isinstance(v, h5py.Dataset):
+                data[k] = v[()]
+            elif isinstance(v, h5py.Group):
+                data[k] = loadmat_group(v)
+    return data
 
 def loadmat_group(group):
     """
