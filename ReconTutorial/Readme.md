2a6bd78518edbfdcb258590a2ab84b3273de66cb
Mobbyjj <fanwen.wang@imperial.ac.uk>
2023-07-04 12:31:11 +0100

    add Readme and main

---
+++ ReconTutorial/Readme.md
@@ -0,0 +1,21 @@
+# MRI Data Loader and Transforms
+This directory provides a reference data loader to read the CMRxRecon data one
+slice and one frame at a time and some useful data transforms to work with the data in
+PyTorch.
+
+Each partition (train, validation or test) of the CMRxRecon data is distributed
+as a set of HDF5 files, such that each HDF5 file contains data from one case with multiple slices and time frames. The set of fields and attributes in these HDF5 files depends on the track (single-coil or multi-coil) and the data partition.
+
+## main
+You can turn to main.py to see the demo recon using fastMRI or turn to CineMapping.ipynb to see the visualisation directly.
+
+
+## functions
+We acknowledged fastMRI repo for their utlity functions to convert k-space into image space. These functions work on PyTorch Tensors. The to_tensor function can convert Numpy arrays to PyTorch Tensors.
+
+'readfile2numpy' read the file from hdf5 to numpy complex
+'show_coils' plots several coil images 
+'fastmri.data.transform' converts from numpy array to pytorch tensor
+'fastmri.ifft2c' applies Inverse Fourier Transform to get the complex image
+'fastmri.complexabs' computes absolute value to get a real image
+'fastmri.rss' gives the coil-combined images with the specified coil dim.
---
+++ ReconTutorial/main.py
@@ -0,0 +1,49 @@
+
+# This script shows how the demo for the multi-coil recon.
+import h5py
+import numpy as np
+from matplotlib import pyplot as plt
+
+import fastmri
+from fastmri.data import transforms as T
+
+# load the file
+def readfile2numpy(file_name):
+    hf = h5py.File(file_name)
+    keys = list(hf.keys())
+    assert len(keys) == 1, f"Expected only one key in file, got {len(keys)} instead"
+    new_value = hf[keys[0]][()]
+    data = new_value["real"] + 1j*new_value["imag"]
+    return data
+
+
+def show_coils(data, slice_nums, cmap=None, vmax = 0.0005):
+    fig = plt.figure()
+    for i, num in enumerate(slice_nums):
+        plt.subplot(1, len(slice_nums), i + 1)
+        plt.imshow(data[num], cmap=cmap,vmax=vmax)
+
+
+# here show the filepath of the multi-coil data
+file_name = '/media/NAS_CMR/CMRxRecon/ChallengeData/MultiCoil/Cine/TrainingSet/FullSample1/P001/cine_sax.mat'
+
+# read files from hdf5 to numpy
+fullmulti = readfile2numpy(file_name)
+
+# choose one slice
+slice_kspace = fullmulti[0,5] 
+
+# Convert from numpy array to pytorch tensor
+slice_kspace2 = T.to_tensor(slice_kspace)       
+# Apply Inverse Fourier Transform to get the complex image       
+slice_image = fastmri.ifft2c(slice_kspace2)     
+# Compute absolute value to get a real image      
+slice_image_abs = fastmri.complex_abs(slice_image)   
+
+
+show_coils(slice_image_abs, [0, 3, 6], cmap='gray', vmax = 0.0005)
+# combine the coil images to a coil-combined ones.
+slice_image_rss = fastmri.rss(slice_image_abs, dim=0)
+
+# plot the final images.
+plt.imshow(np.abs(slice_image_rss.numpy()), cmap='gray', vmax = 0.0015)
